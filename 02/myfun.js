742f2f6f14a3692291b5a968af8364f936dec75a
Mohd Ajlal <ajlal00786@gmail.com>
2024-07-03 00:31:10 +0530

    functions

--- 02/myfun.js
+++ 02/myfun.js
@@ -4,3 +4,9 @@ function addThree(num) {
     return num + 3;
 }
 addThree(5);
+// addThree("4") // error 
+function signUp(name, email, password, sign) {
+}
+var logInUser = function (email, password, log) {
+    if (log === void 0) { log = false; }
+};
--- 02/myfun.ts
+++ 02/myfun.ts
@@ -5,6 +5,10 @@ function addThree(num:number){
 addThree(5)
 // addThree("4") // error 
 
+function signUp(name:string, email:string, password :string, sign:boolean){
 
+}
+
+let logInUser = (email:string, password:string, log:boolean = false) => {}
 
 export {}
