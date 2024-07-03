73956e51c58cd926fc2b1c7eba995eb10c75bd70
Mohd Ajlal <ajlal00786@gmail.com>
2024-07-04 03:19:11 +0530

    type aliases

--- 03_Object/myObject.ts
+++ 03_Object/myObject.ts
@@ -5,11 +5,33 @@ const User = {
     isLogin: false
 }
 
-function createUser({name:string, age:number, isLogin:boolean}){}
-createUser(User);
+// function createUser({name:string, age:number, isLogin:boolean}){}
+// createUser(User);
 
 function createCourse():{name:string, price:number}{
     return {name: "TypeScript", price: 6969}
 }
+
+// type aliases
+
+type MyString = string;
+
+type User = {
+    name: string,
+    email: string,
+    age: number,
+    isLogin: boolean
+}
+
+function createUser(user:User):User{
+return {name:user.name, email:user.email, age:user.age, isLogin:user.isLogin}
+}
+
+createUser({
+    name: "Ajlal",
+    email: "ajlal00786@gmail.com",
+    age: 21,
+    isLogin: false
+})
 
 export {}
